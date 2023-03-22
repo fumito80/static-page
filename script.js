4f7afc174c9e1f1b7e49fc9a3d13cb37702ac7a5
fumito80 <3684217+fumito80@users.noreply.github.com>
2023-03-22 18:35:16 +0900

    fix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementsByTagName('button').addEventListener(() => {
+    document.querySelector('button').addEventListener(() => {
         postMessage('hehehe', '*');
     });
 });
