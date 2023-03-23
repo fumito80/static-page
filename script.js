9d9c7b3fd3b784399bb16d3e828d2903da364b9b
fumito80 <3684217+fumito80@users.noreply.github.com>
2023-03-23 18:09:38 +0900

    done

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('button').addEventListener('click', () => {
         postMessage({ text: 'hehehe', number: 777 }, 'https://fumito80.github.io');
     });
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.moveTo(0, 0);
+    ctx.lineTo(50, 50);
 });
 
 function beginCapture(msg) {
